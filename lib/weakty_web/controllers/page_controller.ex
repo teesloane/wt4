8164cd3b69e169f52fe94400162d99deeb24a4c6
Ty <tylersloane@gmail.com>
2026-02-27 20:55:10 -0500

    Squash merge worktree-homepage into main

--- lib/weakty_web/controllers/page_controller.ex
+++ lib/weakty_web/controllers/page_controller.ex
@@ -1,7 +1,34 @@
 defmodule WeaktyWeb.PageController do
   use WeaktyWeb, :controller
+  require Ash.Query
 
   def home(conn, _params) do
-    render(conn, :home)
+    posts =
+      Weakty.Posts.Post
+      |> Ash.Query.filter(status == :published and post_type == :post)
+      |> Ash.Query.sort(published_at: :desc)
+      |> Ash.Query.limit(5)
+      |> Ash.read!()
+
+    currently_reading =
+      Weakty.MediaLogs.MediaLog
+      |> Ash.Query.filter(media_type == :book and status == :consuming and public == true)
+      |> Ash.Query.sort(date_started: :desc)
+      |> Ash.Query.limit(1)
+      |> Ash.read!()
+      |> List.first()
+
+    recent_tils =
+      Weakty.Tils.Til
+      |> Ash.Query.filter(public == true)
+      |> Ash.Query.sort(published_at: :desc)
+      |> Ash.Query.limit(5)
+      |> Ash.read!()
+
+    render(conn, :home,
+      posts: posts,
+      currently_reading: currently_reading,
+      recent_tils: recent_tils
+    )
   end
 end
